700d1e140d0ae1a9330ec7a40b6949a828c4f22a
David <david.dmr@gmail.com>
2017-04-24 19:05:35 -0300

    Beginning of the project

---
+++ src/main/java/com/david/FanMovieApplication.java
@@ -0,0 +1,12 @@
+package com.david;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanMovieApplication.class, args);
+	}
+}
