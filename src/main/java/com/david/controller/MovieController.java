94c2f78b6bd4f9162dc718645d053b84f7217555
David <david.dmr@gmail.com>
2017-04-27 00:58:00 -0300

    Implementação de controller para Movie

---
+++ src/main/java/com/david/controller/MovieController.java
@@ -0,0 +1,68 @@
+package com.david.controller;
+
+import com.david.model.GenreMovie;
+import com.david.model.Movie;
+import com.david.repository.GenreMovieRepository;
+import com.david.repository.MovieRepository;
+import com.david.utils.GenreNames;
+import com.david.utils.MovieJsons;
+import com.david.utils.ResultJsonAPI;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by david on 26/04/2017.
+ */
+
+@RestController
+@RequestMapping("/movie")
+public class MovieController {
+
+    @Autowired
+    MovieRepository repository;
+
+    @Autowired
+    GenreMovieRepository genreMovieRepository;
+
+    @RequestMapping
+    public String getMovie(@RequestParam("id") Long id) {
+        String result;
+        Movie movie = repository.findOne(id);
+        result = new Gson().toJson(movie);
+
+        return result;
+    }
+
+    @RequestMapping(value = "/fill")
+    public String fillMovieTable() {
+
+        MovieJsons movieUtils = new MovieJsons();
+        List<String> moviesList = new ArrayList<>();
+        moviesList.add(movieUtils.getMoviesPage1());
+        moviesList.add(movieUtils.getMoviesPage2());
+
+        for(String movieItem : moviesList) {
+
+            ResultJsonAPI movieJsonAPI = new Gson().fromJson(movieItem, ResultJsonAPI.class);
+
+            for (Movie movie : movieJsonAPI.getResults()) {
+                repository.saveAndFlush(movie);
+
+                for (long genreId : movie.getGenreIds()) {
+                    String genreName = new GenreNames().getGenreNameById(genreId);
+                    genreMovieRepository.saveAndFlush(new GenreMovie(movie.getId(), genreName));
+                }
+
+            }
+        }
+
+        return "Done!";
+    }
+
+}
