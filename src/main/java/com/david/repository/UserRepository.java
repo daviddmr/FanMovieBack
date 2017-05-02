c573ea77c858b733b3cc9a6bdc8489c81cda5a11
David <david.dmr@gmail.com>
2017-05-02 17:50:47 -0300

    Criação de serviço para retornar usuário através do username

--- src/main/java/com/david/repository/UserRepository.java
+++ src/main/java/com/david/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by david on 28/04/2017.
  */
-public interface UserRepository extends JpaRepository<User, Long> { }
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
