78808c6278f2463e364e66f9423437c0cdace1f0
David <david.dmr@gmail.com>
2017-05-02 20:54:14 -0300

    Criação da entidade Role para credenciamento de permissões na base de dados

---
+++ src/main/java/com/david/model/Role.java
@@ -0,0 +1,31 @@
+package com.david.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by david on 02/05/2017.
+ */
+
+@Entity
+public class Role implements GrantedAuthority{
+
+    @Id
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.name;
+    }
+}
