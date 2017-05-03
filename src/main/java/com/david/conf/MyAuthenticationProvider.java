ee3ad95aaa724d1a15e17b16503a089dbf88a0fa
David <david.dmr@gmail.com>
2017-05-03 20:56:14 -0300

    Alteração de medidas de segurança do sprint security para uso de authenticatorProvider customizado

---
+++ src/main/java/com/david/conf/MyAuthenticationProvider.java
@@ -0,0 +1,56 @@
+package com.david.conf;
+
+import com.david.model.User;
+import com.david.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by david on 03/05/2017.
+ */
+
+@Component
+public class MyAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String teste = authentication.getName();
+        User userFromRepository = userRepository.findByUsername(teste);
+        if(userFromRepository != null){
+            if(userFromRepository.getPassword().equals(authentication.getCredentials())){
+                List<GrantedAuthority> grantedAuths = new ArrayList<GrantedAuthority>();
+                grantedAuths.add(new SimpleGrantedAuthority(userFromRepository.getRoles().get(0).getName()));
+
+                return new UsernamePasswordAuthenticationToken(
+                        authentication.getName(),
+                        authentication.getCredentials(),
+                        grantedAuths);
+            }else{
+                throw new BadCredentialsException("invalid password!");
+            }
+
+        }else{
+            throw new UsernameNotFoundException("unknown username");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+
+}
--- src/main/java/com/david/conf/SecurityConfiguration.java
+++ src/main/java/com/david/conf/SecurityConfiguration.java
@@ -1,17 +1,11 @@
 package com.david.conf;
 
-import com.david.model.User;
-import com.david.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
@@ -19,36 +13,26 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  */
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Autowired
-    UserRepository userRepository;
+    private MyAuthenticationProvider authenticationProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/movie/list").permitAll()
                 .antMatchers("/user/signup").permitAll()
+                .antMatchers("/user/logout").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin()
+                .and().httpBasic()
                 .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
                 .and().csrf().disable();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(new UserDetailsService() {
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                User user = userRepository.findByUsername(username);
-                if (user != null) {
-                    return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.isEnabled(), user.isAccountNonExpired(), user.isCredentialsNonExpired(), user.isAccountNonLocked()
-                            , user.getRoles());
-                } else {
-                    throw new UsernameNotFoundException(
-                            "O usuário " + username + "não existe.");
-                }
-            }
-        });
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("admin").password("pass").roles("ADMIN");
+        auth.authenticationProvider(authenticationProvider);
     }
 }
