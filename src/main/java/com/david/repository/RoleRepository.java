2cdfabd63fd544488cabb0c3252e537439f127f1
David <david.dmr@gmail.com>
2017-05-02 21:02:34 -0300

    Criação de repository para role, para inserir roles iniciais ao startar o server

--- src/main/java/com/david/repository/RoleRepository.java
+++ src/main/java/com/david/repository/RoleRepository.java
@@ -1,7 +1,10 @@
 package com.david.repository;
 
+import com.david.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * Created by david on 02/05/2017.
  */
-public interface RoleRepository {
+public interface RoleRepository extends JpaRepository<Role, String> {
 }
