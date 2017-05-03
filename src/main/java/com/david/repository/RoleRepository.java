02179aed9df2082735890de8033ba7e4f8c443ea
David <david.dmr@gmail.com>
2017-05-03 20:53:45 -0300

    Criação de retorno de role por nome no repositório

--- src/main/java/com/david/repository/RoleRepository.java
+++ src/main/java/com/david/repository/RoleRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by david on 02/05/2017.
  */
 public interface RoleRepository extends JpaRepository<Role, String> {
+    Role findByName(String rolename);
 }
